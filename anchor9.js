07c81fc56d6396bce53ba3ad5727a36b2043bf66
Herby Herado <herby@Herbys-MacBook-Pro.local>
2017-12-21 13:47:31 +0700

    resubmit task week 2 day 3

--- anchor9.js
+++ anchor9.js
@@ -1,21 +1,21 @@
 //------------------------TUGAS 1------------------------
 
-let nama;
-let angkaPembilang;
-let angkaPenyebut;
+let nama = 'Herby';
+let angkaPembilang = 9;
+let angkaPenyebut = 3;
 let hasilBagi = angkaPembilang / angkaPenyebut;
 console.log(`Halo ${nama}, ${angkaPembilang} dibagi ${angkaPenyebut} adalah sama dengan ${hasilBagi}`);
 
 //------------------------TUGAS 2------------------------
 
-let alas;
-let tinggi;
+let alas = 10;
+let tinggi = 2;
 let luasSegitiga = alas * tinggi / 2;
 console.log(`Luas Segitiga: ${luasSegitiga}`)
 
 //------------------------TUGAS 3------------------------
 
-let tahun;
+let tahun = 2020;
 if (tahun % 4 == 0){
     if (tahun % 100 != 0){
         console.log(`${tahun} adalah tahun kabisat`)
